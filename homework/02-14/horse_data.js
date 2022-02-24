60a32c9c0d5810348f6655d1929e307d6f47cd4a
danjefferies <danjefferies2001@gmail.com>
2022-02-24 14:05:08 -0500

    added homework 3 redo, push homework 4

---
+++ homework/02-14/horse_data.js
@@ -0,0 +1,94 @@
+var dataStart = [
+    {
+      horse: "red",
+      value: 21,
+      yvalue: 90,
+    },
+    {
+      horse: "blue",
+      value: 19,
+      yvalue: 110
+    },
+    {
+      horse: "yellow",
+      value: 18,
+      yvalue: 120,
+    }
+  ];
+
+var dataMid2 = [
+    {
+      horse: "red",
+      value: 17,
+      yvalue: 300
+    },
+    {
+      horse: "blue",
+      value: 18,
+      yvalue: 290,
+    },
+    {
+      horse: "yellow",
+      value: 20,
+      yvalue: 320
+    }
+  ];
+  
+var dataMid1 = [
+    {
+      horse: "red",
+      value: 21,
+      yvalue: 320
+    },
+    {
+      horse: "blue",
+      value: 30,
+      yvalue: 315
+    },
+    {
+      horse: "yellow",
+      value: 25,
+      yvalue: 350
+    }
+  ];
+
+  var dataMid = [
+    {
+      horse: "red",
+      value: 65,
+      yvalue: 320
+    },
+    {
+      horse: "blue",
+      value: 80,
+      yvalue: 330
+    },
+    {
+      horse: "yellow",
+      value: 55,
+      yvalue: 340
+    }
+  ];
+
+
+  var dataFinish = [
+    {
+      horse: "red",
+      value: 98,
+      yvalue: 320
+    },
+    {
+      horse: "blue",
+      value: 90,
+      yvalue: 340
+    },
+    {
+      horse: "yellow",
+      value: 107,
+      yvalue: 315
+    }
+  ];
+
+
+
+
